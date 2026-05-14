6655c03ecc319768b40129efb38565f63cc3e6f4
Marcio <marciomoura.bridon@gmail.com>
2026-05-14 14:03:00 +0200

    feat: added simulation example for pll and filters

---
+++ pitaya/tests/simulation/second_order_filter_sim_test.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+
+#include "pitaya/second_order_filter.hpp"
+#include "pitaya/simulation/gtest_data_exporter.hpp"
+#include "pitaya/simulation/simulator.hpp"
+
+namespace {
+
+using namespace pitaya;
+using namespace mojito;
+
+class SecondOrderFilterSimTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        filter.configure(10.0, 0.707);
+        sim.register_lambda(duration_t{100e-6}, [this]() {
+            double current_time = sim.get_current_simulation_time_seconds();
+            input = (current_time >= 0.05) ? voltage_pu_t{1.0f} : voltage_pu_t{0.0f};
+            output = filter.update(input);
+        });
+
+        sim.register_signal("input", [this]() { return input.value(); });
+        sim.register_signal("output", [this]() { return output.value(); });
+
+        sim.initialize();
+    }
+
+    simulator sim;
+    second_order_low_pass_filter<voltage_pu_t> filter{100e-6};
+    voltage_pu_t input{0.0f};
+    voltage_pu_t output{0.0f};
+};
+
+TEST_F(SecondOrderFilterSimTest, StepResponse)
+{
+    gtest_exporter exporter(sim);
+    sim.simulate_for(duration_t(0.5));
+}
+
+}  // namespace
---
+++ pitaya/tests/simulation/srf_pll_sim_test.cpp
@@ -0,0 +1,58 @@
+#include <gtest/gtest.h>
+
+#include <cmath>
+
+#include "pitaya/simulation/gtest_data_exporter.hpp"
+#include "pitaya/simulation/simulator.hpp"
+#include "pitaya/srf_pll.hpp"
+
+namespace {
+
+using namespace pitaya;
+using namespace mojito;
+
+class SrfPllSimTest : public ::testing::Test {
+protected:
+    void SetUp() override
+    {
+        pll.configure_nominal_frequency(frequency_t{50.0f});
+        pll.configure_pi_controller(0.5, duration_t{5.5e-3});
+        pll.reset({frequency_pu_t{0.0f}});
+
+        sim.register_lambda(duration_t{100e-6}, [this]() {
+            double t = sim.get_current_simulation_time_seconds();
+            double freq = 50.0;
+            double omega = 2.0 * mojito::pi * freq;
+            ref_angle =
+                angle_wrapped::from_radians(angle_t{static_cast<float>(std::fmod(omega * t, 2.0 * mojito::pi))});
+
+            v_abc = abc<voltage_pu_t>{voltage_pu_t{static_cast<float>(std::cos(omega * t))},
+                voltage_pu_t{static_cast<float>(std::cos(omega * t - 2.0 * mojito::pi / 3.0))},
+                voltage_pu_t{static_cast<float>(std::cos(omega * t + 2.0 * mojito::pi / 3.0))}};
+
+            pll.update(mojito::to_alphabeta(v_abc));
+        });
+
+        sim.register_signal("v_abc", [this]() { return v_abc; });
+        sim.register_signal("measured_frequency", [this]() { return pll.get_estimated_frequency().value(); });
+        sim.register_signal(
+            "reference_angle", [this]() { return ref_angle - angle_wrapped::from_radians(angle_t{pi / 2.0f}); });
+        sim.register_signal(
+            "measured_angle", [this]() { return pll.get_estimated_angle_aligned_phase_a().get_radians(); });
+
+        sim.initialize();
+    }
+
+    simulator sim;
+    srf_pll pll{duration_t{100e-6}};
+    abc<voltage_pu_t> v_abc{voltage_pu_t{0.0f}, voltage_pu_t{0.0f}, voltage_pu_t{0.0f}};
+    angle_wrapped ref_angle{0.0f};
+};
+
+TEST_F(SrfPllSimTest, LockedResponse)
+{
+    gtest_exporter exporter(sim);
+    sim.simulate_for(duration_t(0.2));
+}
+
+}  // namespace
